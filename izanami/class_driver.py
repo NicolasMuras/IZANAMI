0846ced30737786e3eb3586faa176eb29c422dcf
Nicolas Muras <62513805+NicolasMuras@users.noreply.github.com>
2021-02-04 16:31:21 -0300

    Update class_driver.py
    Improvement in the wait_request function to handle dynamic Facebook titles.

--- izanami/class_driver.py
+++ izanami/class_driver.py
@@ -11,15 +11,15 @@ class Driver():
     def get_url(self, url):
         self.driver.get(url)
     
-    def return_results(self):
-        
+    def return_results(self, name):
         try:
-            results = self.driver.find_elements_by_partial_link_text('Mario')
+            search = (name).split()[0]
+            results = self.driver.find_elements_by_partial_link_text(search.capitalize())
             results = self.filter_results(results)
+
             return results
 
         except Exception:
-            self.driver.close()
             print('[!] Cannot return results, try again.')
     
     def filter_results(self, results):
@@ -32,11 +32,24 @@ class Driver():
 
         return profile_links
 
+    def find_text(self, text):
+        try:
+            result = self.driver.find_element_by_xpath("//*[ text() = 'La contraseña que has introducido es incorrecta. ' ]")
+            return result.text
+        except Exception:
+            return "[!] Elemento no localizado."  
 
     # Wait until the web title changes.
-    def wait_request(self, expected):
-        while(self.driver.title != expected):
-            pass
-    
+    def wait_request(self, original_title = 0, expected_title = 0):
+        num = str(self.driver.title[1])
+        title_new = ("({}) {}".format(num, expected_title))
+        
+        if expected_title == 0:
+            while(self.driver.title == original_title):
+                pass
+        else:
+            while(True):
+                if self.driver.title == expected_title or self.driver.title == title_new:
+                    break
+        
 ##############################################################################################################################
-
